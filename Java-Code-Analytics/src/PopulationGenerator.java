af64a6e1e4c30732a0cd52c34a546099b7d31d20
Chase Holland <chase.holland@idesignco.org>
2012-03-06 20:10:13 -0600

    adding empty project?

---
+++ Java-Code-Analytics/src/PopulationGenerator.java
@@ -0,0 +1,16 @@
+
+public class PopulationGenerator {
+
+	public PopulationGenerator() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
