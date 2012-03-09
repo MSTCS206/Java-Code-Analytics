8b95354338a05a81624e5b231bce03a2ada9d372
rwhwilliam <rwhyr4@mst.edu>
2012-03-09 10:11:08 -0600

    Added a rules generator that has a method generateRules() that will return an arraylist of rules (for he is able to pull a set of rules for his work)

---
+++ Java-Code-Analytics/src/CodeAnalyzer/Rules/RulesGenerator.java
@@ -0,0 +1,18 @@
+package CodeAnalyzer.Rules;
+
+import java.util.*;
+
+public class RulesGenerator 
+{
+	public RulesGenerator()
+	{
+		
+	}
+	
+	public ArrayList<Rule> generateRules()
+	{
+		ArrayList<Rule> rules = new ArrayList<Rule>();
+		
+		return rules;
+	}
+}
