a358c4ff1e46b1ae6db67810037f364268ea50f6
rwhwilliam <rwhyr4@mst.edu>
2012-03-13 20:42:17 -0500

    added comments

--- Java-Code-Analytics/src/CodeAnalyzer/Rules/Rule.java
+++ Java-Code-Analytics/src/CodeAnalyzer/Rules/Rule.java
@@ -2,8 +2,13 @@ package CodeAnalyzer.Rules;
 
 public class Rule 
 {
+	//quality metric, LOC, SLOC, etc
 	private QualityMetric metric;
+	
+	//numberical value
 	private int value;
+	
+	//Operator that is used to compare, like >=, <=, etc
 	private Op compare;
 	
 	public Rule(QualityMetric metric, int value, Op compare)
