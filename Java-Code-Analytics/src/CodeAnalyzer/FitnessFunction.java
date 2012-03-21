21da1a78d7aa84209e06e202c35887560bf3c218
rwhwilliam <rwhyr4@mst.edu>
2012-03-21 16:46:27 -0500

    Fixed a few errors

--- Java-Code-Analytics/src/CodeAnalyzer/FitnessFunction.java
+++ Java-Code-Analytics/src/CodeAnalyzer/FitnessFunction.java
@@ -1,31 +1,44 @@
-package CodeAnalyzer.Summary;
+package CodeAnalyzer;
 
+import CodeAnalyzer.Summary.Individual;
 import CodeAnalyzer.Summary.SummaryItem;
-import java.util.list;
+
+import java.util.List;
 
 public class FitnessFunction
 {
-    public FitnessFunction(){
+    public FitnessFunction()
+    {
         
     }
-    public Individual getBestFitness(List<SummaryItem> Example,List<Individual> Generated)
+    
+    public Individual getBestFitness(List<SummaryItem> Example, List<Individual> Generated)
     {
         Individual mostFit = Generated.get(0);
+        
         float bestFitness = 0;
-        for(int Ind=0; Ind < Generated.size(); Ind++){
+        
+        for(int Ind=0; Ind < Generated.size(); Ind++)
+        {
             Individual current = Generated.get(Ind);
             float Sum = 0;
-            for(int i=0;i<Generated.size();i++){
-                if(Example.getSummary().contains(current.getSummary().get(i))){
+            for(int i=0;i<Generated.size();i++)
+            {
+                if(Example.contains(current.getSummary().get(i)))
+                {
                     Sum++;
                 }
             }
-            float currentFitness = (Sum/t+Sum/p)/2;
+            
+            float currentFitness = (Sum/current.getSummary().size()+Sum/Example.size())/2;
+            
             if(Ind==0){
                 bestFitness = currentFitness;
             }
-            if(currentFitness > bestFitness){
-                this.mostFit = current;
+            
+            if(currentFitness > bestFitness)
+            {
+                mostFit = current;
                 bestFitness = currentFitness;
             }
         }
