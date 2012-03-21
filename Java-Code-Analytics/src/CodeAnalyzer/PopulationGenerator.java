064bf27b6ca1e7ca113f17a9304ca4472303b72d
rwhwilliam <rwhyr4@mst.edu>
2012-03-21 14:13:57 -0500

    Finished off the rules generator for it adopts ranges for each type that are appropriate.  Also added a function to the Rules Generator for we can pass in a root node, and get a string representation of it out that is human readable

--- Java-Code-Analytics/src/CodeAnalyzer/PopulationGenerator.java
+++ Java-Code-Analytics/src/CodeAnalyzer/PopulationGenerator.java
@@ -17,9 +17,15 @@ public class PopulationGenerator
 		MetricsParser p = new MetricsParser("C:\\Users\\RyanGamer\\Desktop\\CS206\\JLayer-Metrics.xml");
 		List<SummaryItem> s = p.getFragments();
 		*/
+		
+		RulesGenerator gen = new RulesGenerator(3);
+		System.out.println(gen.returnRuleString(gen.generateRules()));
+		
 		FileChooser c = new FileChooser();
 		c.setSize(500, 350);
 		c.setVisible(true);
+		
+		
 	
 	}
 
