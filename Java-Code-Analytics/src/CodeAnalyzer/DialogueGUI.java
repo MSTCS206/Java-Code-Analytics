6d4832f6ff5d8dea608935cbacefc71d60251803
Chase Holland <chase.holland@idesignco.org>
2012-03-21 20:45:28 -0500

    added some stuff for the dancing penguin

--- Java-Code-Analytics/src/CodeAnalyzer/DialogueGUI.java
+++ Java-Code-Analytics/src/CodeAnalyzer/DialogueGUI.java
@@ -1,6 +1,9 @@
+// Animated Gif: http://www.devdaily.com/blog/post/jfc-swing/use-animated-gif-image-in-jfc-swing-application
+
 package CodeAnalyzer;
 
 import java.awt.BorderLayout;
+import java.awt.GridLayout;
 
 import java.awt.Container;
 
@@ -9,7 +12,6 @@ import javax.swing.JPanel;
 import javax.swing.JLabel;
 import javax.swing.JProgressBar;
 import javax.swing.ImageIcon;
-
 
 public class DialogueGUI extends JFrame
 {
@@ -21,7 +23,13 @@ public class DialogueGUI extends JFrame
 		progressBar.setString("0%");
 
 		JPanel p = new JPanel();
+		p.setLayout(new GridLayout(3,1));
+		p.add(titleLabel, java.awt.BorderLayout.CENTER);
 		p.add(progressBar);
+		
+        //ImageIcon ii = new ImageIcon(this.getClass().getResource("LINDA_PENGUIN_10-27-2001.gif"));
+        //imageLabel.setIcon(ii);
+        //contentPane.add(imageLabel, java.awt.BorderLayout.CENTER);
 		
 		Container cp = getContentPane();
 		cp.add(p, BorderLayout.CENTER);
